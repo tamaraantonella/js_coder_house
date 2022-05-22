b086d099e754396d1f3a90c0a1f95edda9606e7d
tamaraantonella <tamarafrazzetta@gmail.com>
2022-05-22 15:35:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# js_coder_house
---
+++ js/ejercicio.js
@@ -0,0 +1,7 @@
+let entrada = parseInt(prompt("ingrese la cantidad de saludos que desea: "));
+
+
+for (i = 0; i < entrada; i++) {
+    document.write("Hola como estas?");
+    document.write("<br>");
+}
