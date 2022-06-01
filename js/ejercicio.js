4d94d72e7ea6cec46453d39bfd18b3634b2adaa5
tamaraantonella <tamarafrazzetta@gmail.com>
2022-06-01 15:55:27 -0300

    estructura html y sass de webpage

--- js/ejercicio.js
+++
@@ -1,7 +0,0 @@
-let entrada = parseInt(prompt("ingrese la cantidad de saludos que desea: "));
-
-
-for (i = 0; i < entrada; i++) {
-    document.write("Hola como estas?");
-    document.write("<br>");
-}
