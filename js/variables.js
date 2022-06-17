b380691a79eeabac20be125bcc8ed67cffbeeef2
tamaraantonella <tamarafrazzetta@gmail.com>
2022-06-17 13:06:36 -0300

    desafio incorporar eventos ok

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,6 @@
 const saludo = document.getElementById("saludo");
 
-const productos =['zapatos', 'zapatillas', 'botas', 'borcegos'];
+const productos =['Zapatos', 'Zapatillas', 'Botas', 'Borcegos', 'Medias'];
 const carrito = [];
 
 const listadoCalzados = document.getElementById("listado");
