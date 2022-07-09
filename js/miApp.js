f754d770c8c8ea8b1106de57a8805c86f5d7f55a
tamaraantonella <tamarafrazzetta@gmail.com>
2022-07-09 20:51:36 -0300

    entrega coder

--- js/miApp.js
+++ js/miApp.js
@@ -8,7 +8,7 @@ class Producto {
     }
     precioFinal() {
         this.precioSinIva = this.precio
-        this.precio = parseFloat((this.precio * IVA))   
+        this.precio = parseFloat((this.precio * IVA))
     }
 }
 
@@ -28,21 +28,30 @@ function agregandoProductos() {
 
 // agregandoProductos();
 
-//filtro 
+//filtro
 selecTalles.addEventListener('change',()=>{
-    let arrayNuevo = stockProductos.filter(elemento => elemento.talle === selecTalles.value)
-    selecTalles.value === 'all' ? mostrarProductos(stockProductos) : mostrarProductos(arrayNuevo)
+    console.log(selecTalles.value);
+    if(selecTalles.value == 'all'){
+        mostrarProductos(stockProductos)
+    }else{
+        let arrayNuevo = stockProductos.filter(elemento => elemento.talle === selecTalles.value)
+        console.log(arrayNuevo);
+        mostrarProductos(arrayNuevo)
+    }
 })
 
-mostrarProductos(stockProductos)
+//Buscado
 
+// mostrarProductos(stockProductos)
 //logica Ecommerce
 function mostrarProductos(array){
     contenedorProductos.innerHTML = ""
     array.forEach(el => {
         let div = document.createElement('div')
+        // div.className = 'producto'
+        // div.setAttribute('class', 'producto')
         div.classList.add('producto')
-        div.innerHTML= `<div class="card border-light">
+        div.innerHTML= `<div class="card">
                         <img src="${el.img}">
                         <div class="card-body">
                             <span class="card-title">${el.tipo} ${el.nombre} </span>
@@ -51,7 +60,7 @@ function mostrarProductos(array){
                             <p>${el.desc}</p>
                             <p>Talle: ${el.talle}</p>
                             <p>$${el.precio}</p>
-                            <button type="button"  class="btn btn-outline-secondary" id="boton${el.id}">Agregar al carrito<i class="fa-solid fa-cart-shopping"></i></button>
+                            <a id="boton${el.id}" class="btn btn-primary">Agregar al carrito<i class="fa-solid fa-cart-shopping"></i></a>
                         </div>
                     </div> `
         contenedorProductos.appendChild(div)
@@ -60,33 +69,109 @@ function mostrarProductos(array){
             agregarAlCarrito(el.id);
         })
     })
-
-
 }
 
-function agregarAlCarrito(id) {
-    let productoAgregar = stockProductos.find(obj=> obj.id === id)
-    carritoDeCompras.push(productoAgregar)
-    mostrarCarrito(productoAgregar)
+
+
+function mostrarCarrito(id) {
+    let productoAgregar
+    let btnEliminar
+    let div
+    evaluar(id) ? console.log("Ta bien") : (
+    productoAgregar = stockProductos.find(obj=> obj.id == id),
+    carritoDeCompras.push(productoAgregar),
+    div = document.createElement('div'),
+    div.setAttribute('class', 'productoEnCarrito'),
+    div.innerHTML=`<p>${productoAgregar.nombre}</p>
+                    <p id="Contador${id}">1</p>
+                    <p>Precio: $${productoAgregar.precio}</p>
+                    <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`,
+    contenedorCarrito.appendChild(div),
+    btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`),
+    btnEliminar.addEventListener('click',()=>{
+        const el = document.getElementById(`Contador${id}`)
+        el.innerText == 1 ? (
+        btnEliminar.parentNode.remove(),
+        carritoDeCompras = carritoDeCompras.filter((el)=> el.id != productoAgregar.id),
+        actualizarCarrito()
+        ):
+        el.innerText = parseInt(el.innerText) - 1
+        actualizarCarrito()
+        console.log(carritoDeCompras)
+
+    }),
     actualizarCarrito()
-}
+)}
+
 
 function  actualizarCarrito (){
-    contadorCarrito.innerText = carritoDeCompras.length
-    precioTotal.innerText = carritoDeCompras.reduce((acc,el)=> acc + el.precio, 0 )   //acumulador     
-}  
+    cantidad = carritoDeCompras.reduce((acc, el)=>{
+        let cantidad = parseInt(document.getElementById(`Contador${el.id}`).innerHTML) 
+        return acc + cantidad
+    },0)
+    contadorCarrito.innerText = cantidad
+    precioTotal.innerText = carritoDeCompras.reduce((acc,el)=> {
+        let cantidad = document.getElementById(`Contador${el.id}`).innerHTML
+        let total = cantidad * el.precio
+        return acc + total
+    }, 0 )   //acumulador
+}
 
-function mostrarCarrito(productoAgregar) {
-    let div = document.createElement('div')
-    div.setAttribute('class', 'productoEnCarrito')
-    div.innerHTML=`<p>${productoAgregar.nombre}</p>
-                    <p>Precio: $${productoAgregar.precio}</p>
-                    <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
-    contenedorCarrito.appendChild(div)
-    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
-    btnEliminar.addEventListener('click',()=>{
-        btnEliminar.parentElement.remove()
-        carritoDeCompras = carritoDeCompras.filter(elemento => elemento.id !== productoAgregar.id)
-        actualizarCarrito()
+function agregarEventos(){
+    const botones = document.querySelectorAll(`.botonAgregar`)
+    const botonesArray = Array.apply(null, botones)
+    botonesArray.forEach((ele)=>{
+        ele.addEventListener("click", ()=>{
+            mostrarCarrito(ele.parentElement.parentElement.id)
+        })
     })
 }
+
+//FETCH
+const obtContent = (URL) => {
+    fetch(URL) //busca en la url del archivo JSON o servidor, en este caso archivoJSON
+        .then(response => response.json()) // tambien puede ser asi,respuesta = JSON.parse(response)
+        .then(data => {
+            stockProductos = data;
+            stockProductos.forEach(contenido => {
+                cardsAmostrar += retornoCorrecto(contenido)
+            })
+            contenedorProductos.innerHTML = cardsAmostrar;
+            agregarEventos()
+        })
+}
+
+const retornoCorrecto = (content) => {
+    const { nombre, tipo, img, talle, precio, id, desc } = content
+
+    let HTMLCard = `<div id="${id}" class="card">
+                    <img src="${img}">
+                    <div class="card-body">
+                        <span class="card-title">${tipo} ${nombre} </span>
+                    </div>
+                    <div class="card-content">
+                        <p>${desc}</p>
+                        <p>Talle: ${talle}</p>
+                        <p>$${precio}</p>
+                        <a class="btn btn-primary botonAgregar">Agregar al carrito<i class="fa-solid fa-cart-shopping"></i></a>
+                    </div>
+                </div> `
+    return HTMLCard
+
+};
+
+const evaluar = (id) => {
+    let el
+    let valor
+    carritoDeCompras.some(elemento => elemento.id == id) ? (
+        valor = true,
+        el = document.getElementById(`Contador${id}`),
+        el.innerText = parseInt(el.innerText) + 1,
+        actualizarCarrito()
+    ) : valor = false
+    return valor
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    obtContent(URL);
+})
--- js/variables.js
+++ js/variables.js
@@ -1,13 +1,13 @@
 let carritoDeCompras = []
 
-const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorProductos = document.getElementById('contenedor-productos'); //donde van a cargarse los elementos cuando aparezcan
 const contenedorCarrito = document.getElementById('carrito-contenedor');
 
 const botonTerminar = document.getElementById('terminar')
 const finCompra = document.getElementById('fin-compra')
 
 const contadorCarrito = document.getElementById('contadorCarrito');
-const precioTotal = document.getElementById('precioTotal');
+const precioTotal = document.querySelector('.precioProducto');
 
 const selecTalles = document.getElementById('selecTalles')
 const buscador = document.getElementById('search')
@@ -17,4 +17,9 @@ const inputTel = document.querySelector("#inputTelefono")
 const inputEmail = document.querySelector("#inputEmail")
 const btnSubmit = document.querySelector("#submit")
 
-let datosDeInput = ""
+let datosDeInput = "";
+
+const URL = `../js/stock.json`;
+let cardsAmostrar = "";
+let contenidoJSON = []
+let stockProductos = []
